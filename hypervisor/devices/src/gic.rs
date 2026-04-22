5c34f90ca2226b9a4d4b5fa2f6a2ef9348f07f14
luojiyin <luojiyin@hotmail.com>
2026-04-22 19:31:09 +0800

    fix(typo): correct spelling in hypervisor/devices/src/gic.rs
    Autonomously-by: Codex:GPT-5

--- hypervisor/devices/src/gic.rs
+++ hypervisor/devices/src/gic.rs
@@ -26,7 +26,7 @@ type Result<T> = result::Result<T, Error>;
 pub const IRQ_LEGACY_BASE: usize = layout::IRQ_BASE as usize;
 pub const IRQ_LEGACY_COUNT: usize = 32;
 
-// Gic (Generic Interupt Controller) struct provides all the functionality of a
+// Gic (Generic Interrupt Controller) struct provides all the functionality of a
 // GIC device. It wraps a hypervisor-emulated GIC device (Vgic) provided by the
 // `hypervisor` crate.
 // Gic struct also implements InterruptController to provide interrupt delivery
